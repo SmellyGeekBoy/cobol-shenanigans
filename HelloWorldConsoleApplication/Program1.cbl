bf66be3d1c9f1ec2ec5cbbffff44a91b99e1ffd0
Rees Clissold <rees@reecreate.com>
2015-09-08 22:28:56 +0100

    Hello World!

---
+++ HelloWorldConsoleApplication/Program1.cbl
@@ -0,0 +1,11 @@
+       program-id. Program1 as "HelloWorldConsoleApplication.Program1".
+
+       data division.
+       working-storage section.
+
+       procedure division.
+           display 'HELLO WORLD'
+           stop run
+           goback.
+           
+       end program Program1.
